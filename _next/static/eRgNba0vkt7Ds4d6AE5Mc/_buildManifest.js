0693c0030525df37fb87bc7e52e8b32758cd2034
Prajwalprakash3722 <prajwalprakash3722@gmail.com>
2022-02-15 07:33:14 +0000

    Deploying to gh-pages from @ Prajwalprakash3722/quiz-app@4ecc70b325cce8f2fb8724b89c4f48746f540d40 🚀

---
+++ _next/static/eRgNba0vkt7Ds4d6AE5Mc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/647-c1c62ec88847b355.js","static/chunks/pages/index-addfcccc3f57871f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
